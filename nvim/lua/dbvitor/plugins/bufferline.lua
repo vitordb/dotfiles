0dd0587ae9704dd505496e560c0a8267f6608682
Vítor Duarte Buchweitz <dbvitor@gmail.com>
2024-04-05 23:57:09 -0300

    chore: bufferline config

--- nvim/lua/dbvitor/plugins/bufferline.lua
+++ nvim/lua/dbvitor/plugins/bufferline.lua
@@ -1,4 +1,50 @@
 return {
-  -- using lazy.nvim
-  { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons' },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    requires = 'kyazdani42/nvim-web-devicons', -- certifique-se de que 'requires' esteja correto
+    config = function()
+      local bufferline = require 'bufferline' -- Ajuste aqui: defina 'bufferline' corretamente
+      bufferline.setup {
+        options = {
+          mode = 'buffers',
+          style_preset = 'default', -- 'default' é uma string aqui, sem necessidade do bufferline.style_preset
+          themable = true,
+          numbers = 'none',
+          close_command = 'bdelete! %d',
+          right_mouse_command = 'bdelete! %d',
+          left_mouse_command = 'buffer %d',
+          middle_mouse_command = nil,
+          indicator = {
+            icon = '▎',
+            style = 'icon',
+          },
+          hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { 'close' },
+          },
+          buffer_close_icon = '󰅖',
+          modified_icon = '●',
+          close_icon = '',
+          left_trunc_marker = '',
+          right_trunc_marker = '',
+          name_formatter = function(buf) -- altere ou mantenha conforme necessário
+            -- seu código de formatação aqui
+          end,
+          max_name_length = 18,
+          max_prefix_length = 15,
+          truncate_names = true,
+          tab_size = 18,
+          diagnostics = 'nvim_lsp',
+          diagnostics_indicator = function(count, level)
+            local icon = level:match 'error' and ' ' or ''
+            return ' ' .. icon .. count
+          end,
+          -- e assim por diante para as outras opções...
+          -- Coloque suas demais configurações aqui
+        },
+      }
+    end,
+  },
 }
